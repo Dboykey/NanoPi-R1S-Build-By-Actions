f3897755639f6f63e3dafef0706b7e8fa3077aa8
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-02-22 11:14:35 +0800

    Update diy-CK-part3.sh

--- diy-CK-part3.sh
+++ diy-CK-part3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 . ../../remove_unused_config.sh
-cat ../../app_config.seed >> configs/config_h5
-echo '# CONFIG_V2RAY_COMPRESS_UPX is not set' >> configs/config_h5
-sed -i '/docker/Id;/containerd/Id;/runc/Id;/iptparser/Id' configs/config_h5 #fix compile error
+cat ../../app_config.seed >> ../configs/config_h5
+echo '# CONFIG_V2RAY_COMPRESS_UPX is not set' >> ../configs/config_h5
+sed -i '/docker/Id;/containerd/Id;/runc/Id;/iptparser/Id' ../configs/config_h5 #fix compile error
 
 cd ..
 git config --local user.email "action@github.com"
@@ -12,5 +12,5 @@ git config merge.renameLimit 999999
 git remote add upstream https://github.com/coolsnowwolf/lede
 git fetch upstream
 
-cd friendlywrt
-git rebase 0f0c3f1a251f636df68050b75e296c6cc6965590^ --onto upstream/master -X theirs
+#cd friendlywrt
+#git rebase 0f0c3f1a251f636df68050b75e296c6cc6965590^ --onto upstream/master -X theirs
