22f4ae2614ae2d149bed85d9161720128c4d3b0f
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-07 13:34:13 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -21,3 +21,20 @@ cd ../../
 sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/naiveproxy-88.0.4324.96-1/src/base/rand_util_posix.cc
 
 make -j3
+
+# 调整生成SD镜像的脚本
+cd ..
+cp scripts/build.sh scripts/build.sh.bak
+
+# 把使用zip压缩改为使用gzip压缩
+sed -i '297c\\               rm -f F*.gz' scripts/build.sh
+sed -i '298c\\               gzip -9 F*.img' scripts/build.sh
+
+# # 调整输出R1S的脚本，删除重复编译wrt的步骤
+sed -i '130,150 {/build_friendlywrt/d}' scripts/build.sh
+
+# 修改代码让其支持使用其他的wrt源码而不是特定的那套
+sed -i 's/root-allwinner-h5/root-sunxi/' device/friendlyelec/h5/base.mk
+
+# 正式生成SD镜像
+./build.sh nanopi_r1s.mk
