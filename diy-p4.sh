020eb4f3ff5702bead5f06c206eec036deb5cdca
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-09 16:50:59 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -26,13 +26,19 @@ make -j3
 cd ..
 cp scripts/build.sh scripts/build.sh.bak
 
-# 把使用zip压缩改为使用gzip压缩
-#sed -i '/\                rm -f/c\                rm -f F*.gz' scripts/build.sh
-#sed -i '/\                zip/c\                gzip -9 F*.img' scripts/build.sh
+# 删除把img用zip压缩
+sed -i '296,299d' scripts/build.sh
 
 # 调整输出R1S的脚本，删除重复编译wrt的步骤
 #sed -i '130,150 {/build_friendlywrt/d}' scripts/build.sh
 sed -i '130,150 {s/build_friendlywrt/#build_friendlywrt/}' scripts/build.sh
+
+# 为生成SD镜像的脚本加入gzip压缩功能
+sed -i "/space/a\\echo 'RAW image successfully compress'" scripts/sd-fuse/mk-sd-image.sh
+sed -i "/space/a\\gzip -9 {RAW_FILE}" scripts/sd-fuse/mk-sd-image.sh
+sed -i 's/gzip -9 /gzip -9 $/'  scripts/sd-fuse/mk-sd-image.sh
+sed -i "/space/a\\echo '---------------------------------'" scripts/sd-fuse/mk-sd-image.sh
+sed -i "/space/a\\ " scripts/sd-fuse/mk-sd-image.sh
 
 # 修改代码让其支持使用其他的wrt源码而不是特定的那套
 sed -i 's/root-allwinner-h5/root-sunxi/' device/friendlyelec/h5/base.mk
